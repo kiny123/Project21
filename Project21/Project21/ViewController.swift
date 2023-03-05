2666750accb27b59f07c5986d1561ed3bf2fa520
kiny123 <89192734912nik@gmail.com>
2023-03-06 00:42:24 +0300

    init

---
+++ Project21/Project21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project21
+//
+//  Created by nikita on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
